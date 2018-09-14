fd064debe64cb851cc1ace2601879b993b9eb6ac
v-cheye <v-cheye@microsoft.com>
2018-09-14 12:45:48 +0800

    update

---
+++ tests/test_helloworld.py
@@ -0,0 +1,12 @@
+import os
+import pytest
+
+root_dir = os.getcwd()
+module_name = "iotedgeFilterModule"
+file_list = ["requirements.txt", "module.json", "main.py", "Dockerfile.arm32v7",
+             "Dockerfile.amd64.debug", "Dockerfile.amd64", ".gitignore"]
+
+
+
+def test_check_iotedgemodule_exist():
+    assert 1==1
