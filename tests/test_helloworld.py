8adf5ad59abda5b7907ea300e76d07a7f5f692fb
Neil Ye (Pactera Technologies Inc) <v-cheye@microsoft.com>
2018-11-21 10:18:51 +0800

    update

--- tests/test_helloworld.py
+++ tests/test_helloworld.py
@@ -5,8 +5,11 @@ import re
 VALID_DEVICECONNECTIONSTRING = os.environ['DEVICE_CONNECTION_STRING']
 
 
+def test_check_iotedgemodule_exist2():
+    print(os.environ)
+
+
 def test_check_iotedgemodule_exist():
-    os.environ
     device_id = re.findall(
         ".*DeviceId=(.*);SharedAccessKey.*", VALID_DEVICECONNECTIONSTRING)[0]
     print(device_id)
